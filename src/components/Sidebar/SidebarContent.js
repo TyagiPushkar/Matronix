3fdbef6799134e72cae1585efbda1d891c878cef
yashxjain <jyash5332@gmail.com>
2024-12-12 17:26:02 +0530

    mat

--- src/components/Sidebar/SidebarContent.js
+++ src/components/Sidebar/SidebarContent.js
@@ -4,6 +4,7 @@ import { NavLink, Route } from 'react-router-dom'
 import * as Icons from '../../icons'
 import SidebarSubmenu from './SidebarSubmenu'
 import { Button } from '@windmill/react-ui'
+import logo from '../../assets/img/favicon.png' // Ensure correct import path
 
 function Icon({ icon, ...props }) {
   const Icon = Icons[icon]
@@ -13,8 +14,9 @@ function Icon({ icon, ...props }) {
 function SidebarContent() {
   return (
     <div className="py-4 text-gray-500 dark:text-gray-400">
-      <a className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
-        Windmill
+      <a className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200 flex items-center" href="#">
+        <img src={logo} alt="Logo" className="w-8 h-8 mr-2" /> {/* Logo image */}
+        MATRONIX
       </a>
       <ul className="mt-6">
         {routes.map((route) =>
